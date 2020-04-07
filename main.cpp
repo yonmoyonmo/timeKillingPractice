173502dc2ed7e692ad39bf655a4e3ab9acb1d4ff
yonmoyonmo <yonmosound@gmail.com>
2020-04-08 08:15:07 +0900

    saveload

--- main.cpp
+++ main.cpp
@@ -2,15 +2,61 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
+#include <Windows.h>
 
 #include "player.h"
 #include "item.h"
 #include "tool.h"
 
-
+Player& makeNewCharacter() {
+	std::string name;
+	std::cout << "캐릭터의 이름을 입력하라" << std::endl;
+	std::cin >> name;
+	Player* newone = new Player(name);
+	return *newone;
+}
+Player& loadCharacter() {
+	Player* oldone = new Player();
+	return *oldone;
+}
+void printCharacterInfo(Player& player) {
+	std::cout << player.getName() << std::endl;
+	std::cout << player.getMoney() << std::endl;
+	std::cout << player.getMaxAp() << std::endl;
+	std::cout << player.getToolsInven(0) << std::endl;
+}
 
 int main()
 {
+	Player guichana("none");
+	Player& current = guichana;
+	std::cout << "test" << std::endl;
+	std::cout << "=====" << std::endl;
+	int choice;
+	std::cout << "선택하시오" << std::endl;
+	std::cout << "1. 새 캐릭터" << std::endl;
+	std::cout << "2. 불러오기" << std::endl;
+	std::cout << "3. 종료" << std::endl;
+	std::cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		current = makeNewCharacter();
+		break;
+	case 2:
+		current = loadCharacter();
+		break;
+	case 3:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+	printCharacterInfo(current);
+	current.save();
+	Sleep(5000);
+	return 0;
 }
 
 
--- player.h
+++ player.h
@@ -26,8 +26,50 @@ public:
 		std::string character;
 		std::cout << "불러올 캐릭터의 이름을 입력하세요: ";
 		std::cin >> character;
+		std::string _money;
+		std::string _AP;
+
 		std::ifstream load("./save/" + character + ".txt");
+	
+		while (load.peek() != EOF) {
+				std::getline(load, name);
+				std::getline(load, _money);
+				std::getline(load, _AP);
+				std::getline(load, toolInven[0]);
+				std::getline(load, toolInven[1]);
+				for (int i = 0; i < 24; i++) {
+					std::getline(load, itemInven[i]);
+				}
+			}
+		load.close();
+		money = std::stoi(_money);
+		maxAP = std::stoi(_AP);
 
+	}
+	void save() {
+		std::ofstream save("./save/" + name + ".txt");
+		
+		save << name << std::endl;
+		save << money << std::endl;
+		save << maxAP << std::endl;
+		save << toolInven[0] << std::endl;
+		save << toolInven[1] << std::endl;
+		for (int i = 0; i < 24; i++) {
+			save << itemInven[i] << std::endl;
+		}
 
+		save.close();
+	}
+	std::string getName() {
+		return name;
+	}
+	int getMoney() {
+		return money;
+	}
+	int getMaxAp() {
+		return maxAP;
+	}
+	std::string getToolsInven(int choice) {
+		return toolInven[choice];
 	}
 };
